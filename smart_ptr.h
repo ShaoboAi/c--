a45709b595904749058373bf19c6b718d3dbf333
shaobo1989 <shaobo.aliyun@gmail.com>
2014-06-04 16:41:56 +0800

    smart pointer

---
+++ smart_ptr.h
@@ -0,0 +1,54 @@
+
+
+
+template<class T>
+class smart_ptr
+{
+public:
+    smart_ptr();
+
+    smart_ptr(T* value);
+    smart_ptr& operator=(const smart_ptr& sp);
+    ~smart_ptr();
+private:
+    smart_ptr(const smart_ptr& sp);
+    T* p;//T& p;
+    int* counter;
+};
+
+template<class T>
+smart_ptr<T>::smart_ptr():p(NULL),counter(NULL)
+{
+}
+
+template<class T>
+smart_ptr<T>::smart_ptr(T* value):p(value)
+{
+
+    counter = new int();
+    *counter = 1;
+}
+
+template<class T>
+smart_ptr<T>& smart_ptr<T>::operator=(const smart_ptr& sp)
+{
+    if(this != &sp)
+    {
+        counter = sp.counter;
+        p = sp.p;
+        *counter += 1;
+    }
+    return *this;
+}
+
+template<class T>
+smart_ptr<T>::~smart_ptr()
+{
+    if(--*counter == 0 )
+    {
+        delete p;
+        delete counter;
+    }
+
+}
+
